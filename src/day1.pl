32e869b60864edb52469a733c27e1b4792a335e3
Giacomo Sansone <giacomo.sansone@yahoo.com>
2023-12-02 13:28:14 +0100

    Day 1 - Improved Code

--- src/day1.pl
+++ src/day1.pl
@@ -1,9 +1,11 @@
 use warnings;
 use strict;
+use Scalar::Util qw(looks_like_number);
 
-my $sum_values_part_1 = 0;
-my $sum_values_part_2 = 0;
+# Answer variables
+my ($sum_values_part_1, $sum_values_part_2) = (0, 0);
 
+# Hash to search in part 2
 my %values_to_search = (
   "one" => 1,   1 => 1, "two" => 2,   2 => 2, "three" => 3, 3 => 3,
   "four" => 4,  4 => 4, "five" => 5,  5 => 5, "six" => 6,   6 => 6, 
@@ -12,54 +14,37 @@ my %values_to_search = (
 
 while(<>){
 
-  # Part 1 solution
+  my $current_string = $_;
 
-  # Extract first number in the string
-  my ($number_in_string) = $_ =~ /^.*?(\d+)/;
-  # Get most significant digit of the number
-  my $number = (substr "$number_in_string", 0, 1) * 10; 
-  # Extract last number in the string
-  ($number_in_string) = $_ =~ /(\d+)\D*$/;
-  # Gest least significant digit in the number
-  $number = (substr "$number_in_string", -1, 1) + $number; 
-  # Accumulate the number
-  $sum_values_part_1 += $number;
+  # Get first and last digit of the string
+  my $first_number = ($_ =~ /(\d)/g)[0];
+  my $last_number  = ($_ =~ /(\d)/g)[-1];
 
-  # Part 2 solution
+  # Add correct value for part 1
+  $sum_values_part_1 += ($first_number * 10 + $last_number);
 
-  my $max_position = -1;
-  my $min_position = 999;
-  my $max_value = 0;
-  my $min_value = 0;
+  # Initialize variables to detect position of each element in the hash
+  my ($max_pos, $max_val, $min_pos, $min_val) = (-1, 0, ~0, 0);
 
   # Find the occurrances of each key of the map inside the string
   while(my($key_to_search, $value) = each %values_to_search){
-    # left-most occurrence
-    my $left_index = index $_, "$key_to_search";
-    # right-most occurrence
-    my $right_index = rindex $_, "$key_to_search";
+
+    my ($left_index, $right_index) = ((index $_, $key_to_search), (rindex $_, $key_to_search));
 
     # Skip if not found
-    if ($left_index == -1) {
-      next;
-    }
+    if ($left_index == -1) { next; }
 
-    # Possibly update the first position
-    $min_value = ($left_index < $min_position) ? $value : $min_value;
-    $min_position = ($left_index < $min_position) ? $left_index : $min_position;
+    # Possibly update the first element in the string
+    ($min_val, $min_pos) = ($left_index < $min_pos) ? ($value, $left_index) : ($min_val, $min_pos);
 
-    # Possibly update the last position
-    $max_value = ($right_index > $max_position) ? $value : $max_value;
-    $max_position = ($right_index > $max_position) ? $right_index : $max_position;
+    # Possibly update the last element in the string
+    ($max_val, $max_pos) = ($right_index > $max_pos) ? ($value, $right_index) : ($max_val, $max_pos);
 
   }
 
-  $sum_values_part_2 += ($min_value * 10 + $max_value);
+  # Add correct value for part 2
+  $sum_values_part_2 += ($min_val * 10 + $max_val);
 }
 
-print "------- RESULT PART 1 -------\n";
-print "$sum_values_part_1\n";
-
-print "------- RESULT PART 2 -------\n";
-print "$sum_values_part_2\n";
-
+print "------- RESULT PART 1 -------\n$sum_values_part_1\n";
+print "------- RESULT PART 2 -------\n$sum_values_part_2\n";
