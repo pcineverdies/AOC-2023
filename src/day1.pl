8c79e1b7ebd8029d358b9cdd0e8baf16b05ea6ba
Giacomo Sansone <giacomo.sansone@yahoo.com>
2023-12-03 13:52:43 +0100

    Day 1 - Improved Code

--- src/day1.pl
+++ src/day1.pl
@@ -7,21 +7,14 @@ my ($sum_values_part_1, $sum_values_part_2) = (0, 0);
 
 # Hash to search in part 2
 my %values_to_search = (
-  "one" => 1,   1 => 1, "two" => 2,   2 => 2, "three" => 3, 3 => 3,
-  "four" => 4,  4 => 4, "five" => 5,  5 => 5, "six" => 6,   6 => 6, 
-  "seven" => 7, 7 => 7, "eight" => 8, 8 => 8, "nine" => 9,  9 => 9,
+  "one" => 1,   1 => 1, "two" => 2,   2 => 2, "three" => 3, 3 => 3, "four" => 4,  4 => 4, "five" => 5, 
+  5 => 5, "six" => 6,   6 => 6, "seven" => 7, 7 => 7, "eight" => 8, 8 => 8, "nine" => 9,  9 => 9,
 );
 
 while(<>){
 
-  my $current_string = $_;
-
-  # Get first and last digit of the string
-  my $first_number = ($_ =~ /(\d)/g)[0];
-  my $last_number  = ($_ =~ /(\d)/g)[-1];
-
   # Add correct value for part 1
-  $sum_values_part_1 += ($first_number * 10 + $last_number);
+  $sum_values_part_1 += (($_ =~ /(\d)/g)[0] * 10 + ($_ =~ /(\d)/g)[-1]);
 
   # Initialize variables to detect position of each element in the hash
   my ($max_pos, $max_val, $min_pos, $min_val) = (-1, 0, ~0, 0);
@@ -32,7 +25,7 @@ while(<>){
     my ($left_index, $right_index) = ((index $_, $key_to_search), (rindex $_, $key_to_search));
 
     # Skip if not found
-    if ($left_index == -1) { next; }
+    next if ($left_index == -1);
 
     # Possibly update the first element in the string
     ($min_val, $min_pos) = ($left_index < $min_pos) ? ($value, $left_index) : ($min_val, $min_pos);
