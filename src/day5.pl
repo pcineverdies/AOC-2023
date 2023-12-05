422a2adec483821b1afe2a52a8e6b8fe93c2cbb0
Giacomo Sansone <giacomo.sansone@yahoo.com>
2023-12-05 16:03:32 +0100

    Day 5 - Improved Code

--- src/day5.pl
+++ src/day5.pl
@@ -1,36 +1,26 @@
 use warnings; use strict;
 use List::Util qw( min );
 
-my ($result_part_1, $result_part_2) = (0, ~0); # Results
-
-# Store the intervals in the current section in the format [dst, src, size]
-my @current_section_intervals = ();
-
-# Store values for part 1
-my @numbers = ();
-my @converted = ();
-
-# Store ranges for part 2
-my @ranges = ();
-my @converted_ranges = ();
+my ($result_part_1, $result_part_2) = (0, ~0);  # Results
+my @section_intervals = (); # Store the intervals in the current section as [dst, src, size]
+my @numbers = (); my @converted_numbers = ();   # Store values for part 1
+my @ranges = ();  my @converted_ranges = ();    # Store ranges for part 2
 
 while(<>){  
 
   # Initialize the numbers and the ranges for part 1 and 2
   if($_ =~ /seeds/){
+
     # Add all the numbers
-    @numbers = ($_ =~ /(\d+)/g);
+    @numbers = ($_ =~ /(\d+)/g); 
 
-    # Generate all the ranges ans [begin, end)
-    for(my $i = 0; $i < (scalar @numbers); $i+=2)
-      { my @newrange = ($numbers[$i], $numbers[$i] + $numbers[$i+1]); push(@ranges, \@newrange); }
-
+    # Generate all the ranges as [begin, end) in @ranges
+    push(@ranges, ($numbers[$_*2], $numbers[$_*2+1] + $numbers[$_*2])) for(0..$#numbers/2);
     next;
   }
 
-  # If the current line contains numbers, than section intervals must be updated;
-  if($_ =~ /(\d)/)
-    { my @newinterval = $_ =~ /(\d+)/g; push(@current_section_intervals, \@newinterval); }
+  # If the current line contains numbers, than section intervals must be updated; 
+  push(@section_intervals, ($_ =~ /(\d+)/g)) if($_ =~ /(\d)/);
 
   # If the current line is \n or eof(), then we convert the numbers and the ranges
   if($_ eq "\n" || eof()){
@@ -40,44 +30,46 @@ while(<>){
     # For each number available
     for my $num (@numbers){
 
-      # Try to convert it considering all the available ranges in that section
+      # Try to convert it considering all the available intervals in that section
       my $converted_value = $num; 
 
-      foreach my $current_interval (@current_section_intervals){
-        my ($dst_start, $src_start, $size) = @$current_interval;
+      for(my $i = 0; $i < ($#section_intervals); $i+=3){
+        
+        my ($dst_start, $src_start, $size) = @section_intervals[$i .. $i+2];
+
         # If conversion is possible, update $converted_value
         if($num >= $src_start && $num < $src_start + $size)
           { $converted_value = $num - $src_start + $dst_start; last; }
       }
 
       # Push new values into @converted
-      push(@converted, $converted_value);
+      push(@converted_numbers, $converted_value);
     }
 
     # Update numbers and reset converted
-    @numbers = @converted; @converted = ();
+    @numbers = @converted_numbers; @converted_numbers = ();
 
     # --- PART 2 ---
 
     # For each range still available
-    while(scalar @ranges > 0){
+    while($#ranges > 0){
 
-      # Extract one range from the array
-      my $range = pop(@ranges);
-      # Stores if a conversion has been done (if not, the whole range remains the same)
+      # Extract starting and ending point of current range
+      my ($range_end, $range_start) = (pop(@ranges), pop(@ranges));
+
+      # Stores if a conversion has been done (if not, the whole range remains the same in next step)
       my $conversion_happened = 0;
-      # Get begin and end of current range;
-      my ($range_start, $range_end) = @$range;
 
       # For each interval in the section
-      for my $current_interval (@current_section_intervals){
+      for(my $i = 0; $i < (scalar @section_intervals); $i+=3){
 
-        # Extract content and compute the end of the interval
-        my ($dst_start, $src_start, $size) = @$current_interval;
+        # Extract interval and compute the end of the interval
+        my ($dst_start, $src_start, $size) = @section_intervals[$i .. $i+2];
         my $src_end = $src_start + $size;
 
         # Compute the position of the beginning of the overlap
         my $overlap_start = ($range_start > $src_start) ? $range_start : $src_start;
+
         # Compute the position of the end of the overlap
         my $overlap_end = ($range_end < $src_end) ? $range_end : $src_end;
 
@@ -85,34 +77,28 @@ while(<>){
         if($overlap_start < $overlap_end){
 
           # The region [overlap_start, overlap_end] must be converted
-          my @newrange = ($overlap_start - $src_start + $dst_start, $overlap_end - $src_start + $dst_start);
-          push(@converted_ranges, \@newrange);
+          push(@converted_ranges, ($overlap_start - $src_start + $dst_start, $overlap_end - $src_start + $dst_start));
 
           # If one initial region is out of the interval, than add it back to ranges
-          if($overlap_start > $range_start)
-            { my @newrange = ($range_start, $overlap_start); push(@ranges, \@newrange); }
+          push(@ranges, ($range_start, $overlap_start)) if($overlap_start > $range_start);
 
           # If one final region is out of the interval, than add it back to ranges
-          if($range_end > $overlap_end)
-            { my @newrange = ($overlap_end, $range_end); push(@ranges, \@newrange); }
+          push(@ranges, ($overlap_end, $range_end)) if($range_end > $overlap_end);
 
           # Conversion has been done
-          $conversion_happened = 1;
-          last;
+          $conversion_happened = 1; last;
         }
       }
 
       # If no conversion has been done, then the whole range enters as it is
-      if(not $conversion_happened)
-        { my @newrange = ($range_start, $range_end); push(@converted_ranges, \@newrange); }
+      push(@converted_ranges, ($range_start, $range_end)) if(not $conversion_happened);
     }
 
     # Update ranges and reset converted ranges
-    @ranges = @converted_ranges;
-    @converted_ranges = ();
+    @ranges = @converted_ranges; @converted_ranges = ();
 
     # Reset ranges for the section
-    @current_section_intervals = ();
+    @section_intervals = ();
   }
 }
 
@@ -120,7 +106,7 @@ while(<>){
 $result_part_1 = min @numbers;
 
 # Compute minimum of fist elements of each range as result for part 2
-foreach my $range (@ranges)
-  { $result_part_2 = $$range[0] if ($$range[0] < $result_part_2); }
+($ranges[$_*2] < $result_part_2) and $result_part_2 = $ranges[$_*2] for (0..$#ranges/2);
+
 
 print "Part 1: $result_part_1\nPart 2: $result_part_2\n"
