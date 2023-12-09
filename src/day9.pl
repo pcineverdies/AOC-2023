a7d997ae89516d391b12e1e71428755f171199ca
Giacomo Sansone <giacomo.sansone@yahoo.com>
2023-12-09 08:57:40 +0100

    Day 9 - Saved some LOCs

--- src/day9.pl
+++ src/day9.pl
@@ -13,30 +13,26 @@ while(<>){
 
     # Compute new line
     my @new_line = ();
-    push(@new_line, $current[$_+1] - $current[$_]) for(0..scalar @current - 2);
+    push(@new_line, $current[$_+1] - $current[$_]) for(0..$#current - 1);
 
     # End if new line is all zeros
     last if (all { $_ == 0 } @new_line); @current = @new_line;
   }
 
-  # Utilities for second part: list of first elements, line and temporary result
-  my @first_row = (); @current = @line; my $extrap = 0;
+  # Reset row for part 2
+  @current = @line; my $iteration = 0;
 
   while(1){
-    # Store current first value in the row
-    push(@first_row, $current[0]);
+    # Compute result by adding in case of even iteration, substructing otherwise
+    $result_part_2 += ($iteration++ % 2 == 0) ? $current[0] : -$current[0]; 
 
     # Compute new line
     my @new_line = ();
-    for(my $i = scalar @current - 1; $i >= 1; $i--) { unshift(@new_line, $current[$i] - $current[$i-1]); }
+    for(my $i = $#current; $i >= 1; $i--) { unshift(@new_line, $current[$i] - $current[$i-1]); }
 
     # End if new line is all zeros
     last if (all { $_ == 0 } @new_line); @current = @new_line;
   }
-
-  # Compute extrapolated value for the current line and accumulate it to result
-  for(my $i = scalar @first_row - 1; $i >= 0; $i --) { $extrap = $first_row[$i] - $extrap; }
-  $result_part_2 += $extrap; 
 }
 
 print "Part 1: $result_part_1\nPart 2: $result_part_2\n"
